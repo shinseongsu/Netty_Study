c9fe1ac06dda923f7e67efb4c6655e990413b32c
Shin seongsu <tlstjdtn321@naver.com>
2020-12-19 20:11:59 +0900

    소켓 채널 SO_LINGER

---
+++ ch1/src/main/java/BootStrap/EchoServerV4.java
@@ -0,0 +1,33 @@
+package BootStrap;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServerV4 {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .childOption(ChannelOption.SO_LINGER, 0)
+             .childHandler(new ChannelInitializer<SocketChannel>() {
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                     ChannelPipeline p = ch.pipeline();
+                     p.addLast(new EchoServerHandler());
+                 }
+             });
+            ChannelFuture f = b.bind(8888).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
