8708c16bd8f382b753d058f8ebf1f0c05aafd926
Shin seongsu <tlstjdtn321@naver.com>
2020-12-19 20:09:43 +0900

    EchoServerHandler

---
+++ ch1/src/main/java/BootStrap/EchoServerHandler.java
@@ -0,0 +1,25 @@
+package BootStrap;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+@SharableEchoServerHandler.java
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
