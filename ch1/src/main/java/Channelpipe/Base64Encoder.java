df38d9638aa476efb39098b1d8ad2794a6411873
Shin seongsu <tlstjdtn321@naver.com>
2020-12-21 20:13:22 +0900

    Base64Encoder 내부 모습

---
+++ ch1/src/main/java/Channelpipe/Base64Encoder.java
@@ -0,0 +1,37 @@
+package Channelpipe;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.base64.Base64;
+import io.netty.handler.codec.base64.Base64Dialect;
+
+import java.util.List;
+
+public class Base64Encoder extends MessageToMessageEncoder<ByteBuf> {
+
+    private final boolean breakLines;
+    private final Base64Dialect dialect;
+
+    public Base64Encoder() {
+        this(true);
+    }
+
+    public Base64Encoder(boolean breakLines) {
+        this(breakLines, Base64Dialect.STANDARD);
+    }
+
+    public Base64Encoder(boolean breakLines, Base64Dialect dialect) {
+        if (dialect == null) {
+            throw new NullPointerException("dialect");
+        }
+
+        this.breakLines = breakLines;
+        this.dialect = dialect;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+        out.add(Base64.encode(msg, msg.readerIndex(), msg.readableBytes(), breakLines, dialect));
+    }
+}
