4a25adfc246141131bfa5dc0a4c92051fdb3e837
Shin seongsu <tlstjdtn321@naver.com>
2020-12-21 20:19:10 +0900

    이벤트 핸들러로 이벤트 넘겨주기

---
+++ ch1/src/main/java/Channelpipe/EchoServerV4SecondHandler.java
@@ -0,0 +1,28 @@
+package Channelpipe;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+
+public class EchoServerV4SecondHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf readMessage = (ByteBuf) msg;
+        System.out.println("SecondHandler channelRead : " + readMessage.toString(Charset.defaultCharset()));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        System.out.println("channelReadComplete 발생");
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
