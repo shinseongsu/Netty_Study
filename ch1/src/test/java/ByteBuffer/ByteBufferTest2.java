1097c10029ce31d58547347d1c93afd1fa01f05e
Shin seongsu <tlstjdtn321@naver.com>
2020-12-25 22:30:07 +0900

    바이트 버퍼 오버플로우

---
+++ ch1/src/test/java/ByteBuffer/ByteBufferTest2.java
@@ -0,0 +1,17 @@
+package ByteBuffer;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferTest2 {
+    public static void main(String[] args) {
+        ByteBuffer firstBuffer = ByteBuffer.allocate(11);
+        System.out.println("초기 상태 : " + firstBuffer);
+
+        byte[] source = "Hello world!".getBytes();
+
+        for(byte item : source) {
+            firstBuffer.put(item);
+            System.out.println("현재 상태 : " + firstBuffer);
+        }
+    }
+}
