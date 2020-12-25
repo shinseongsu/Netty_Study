1f478a881fec0ee3a417bfe0ca3647f2743c1089
Shin seongsu <tlstjdtn321@naver.com>
2020-12-25 22:31:20 +0900

    정상적인 바이트 버퍼의 데이터 쓰기와 읽기 테스트

---
+++ ch1/src/test/java/ByteBuffer/RightByteBufferTest3.java
@@ -0,0 +1,30 @@
+package ByteBuffer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.nio.ByteBuffer;
+
+public class RightByteBufferTest3 {
+
+    @Test
+    public void test() {
+        ByteBuffer firstBuffer = ByteBuffer.allocate(11);
+        System.out.println("초기 상태 : " + firstBuffer);
+
+        firstBuffer.put((byte) 1);
+        System.out.println(firstBuffer.get());
+        System.out.println(firstBuffer);
+        assertEquals(2, firstBuffer.position());
+
+        firstBuffer.rewind();
+        assertEquals(0, firstBuffer.position());
+
+        assertEquals(1, firstBuffer.get());
+        assertEquals(1, firstBuffer.position());
+
+        System.out.println(firstBuffer);
+
+    }
+
+}
