f0b6a408c92b489f2b22a557ff10286c6a048f8c
Shin seongsu <tlstjdtn321@naver.com>
2020-12-29 23:18:10 +0900

    TelnetServer Version2

---
+++ ch2/src/main/java/com/example/config/TelnetServerV2.java
@@ -0,0 +1,43 @@
+package com.example.config;
+
+import com.example.TelnetServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+
+@Component
+public class TelnetServerV2 {
+
+    @Autowired
+    @Qualifier("tcpSocketAddress")
+    private InetSocketAddress port;
+
+    public void start() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TelnetServerInitializer());
+
+            ChannelFuture future = b.bind(port).sync();
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
