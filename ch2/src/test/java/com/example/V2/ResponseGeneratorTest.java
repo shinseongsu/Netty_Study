4eeb039541448e53ba59d022149a290f5f13f29b
Shin seongsu <tlstjdtn321@naver.com>
2020-12-30 23:15:37 +0900

    Hi, bye test

--- ch2/src/test/java/com/example/V2/ResponseGeneratorTest.java
+++ ch2/src/test/java/com/example/V2/ResponseGeneratorTest.java
@@ -17,4 +17,30 @@ public class ResponseGeneratorTest {
         assertEquals("명령을 입력해 주세요.\r\n", generator.response());
         assertFalse(generator.isClose());
     }
+
+    @Test
+    public void testHi() {
+        String request = "hi";
+
+        ResponseGenerator generator = new ResponseGenerator(request);
+        assertNotNull(generator);
+
+        assertNotNull(generator.response());
+        assertEquals("입력하신 명령이 '" + request + "' 입니까?\r\n", generator.response());
+
+        assertFalse(generator.isClose());
+    }
+
+    @Test
+    public void testBye() {
+        String request = "bye";
+
+        ResponseGenerator generator = new ResponseGenerator(request);
+        assertNotNull(generator);
+
+        assertNotNull(generator.response());
+        assertEquals("좋은 하루 되세요!\r\n", generator.response());
+        assertTrue(generator.isClose());
+    }
+
 }
