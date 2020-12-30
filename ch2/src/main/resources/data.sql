b7dd43ef94c4af5477ce068d685a8fc2dbad4675
Shin seongsu <tlstjdtn321@naver.com>
2020-12-30 23:18:09 +0900

    h2에 테이블 및 데이터값 추가 SQL

---
+++ ch2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into users ("USERID", "USERNAME", "PASSSWORD") values ('foo@foobar.com', 'foobar', 'passWords');
+insert into users ("USERID", "USERNAME", "PASSSWORD") values ('kris@foobar.com', 'kris jeong', 'zmfltm');
+insert into users ("USERID", "USERNAME", "PASSSWORD") values ('ace@foobar.com', '김일등', 'numberOne');
+insert into users ("USERID", "USERNAME", "PASSSWORD") values ('amy@foobar.com', 'amy lim', '');
+insert into users ("USERID", "USERNAME", "PASSSWORD") values ('emma@foobar.com', '엠마', 'emma');
